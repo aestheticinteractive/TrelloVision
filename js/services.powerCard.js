1becba53d1b093cc75c5ebdae3cab8387274aa70
zachkinstner <zach@aestheticinteractive.com>
2013-05-31 21:19:43 -0400

    Made several PowerCard improvements.

--- js/services.powerCard.js
+++ js/services.powerCard.js
@@ -27,6 +27,29 @@ TrelloVisionApp.factory('PowerCardService', function() {
 		scope.getDueFull = function(d) {
 			return (d == null ? null : moment(d).format('dddd, MMM D, YYYY [at] h:mma'));
 		};
+		
+		scope.getLabelTextColor = function(c) {
+			return (c == "yellow" ? "#555" : "#fff");
+		}
+		
+		scope.getChecklistProg = function(cl, type) {
+			var count = cl.checkItems.length;
+			var comp = 0;
+			
+			for ( ci in cl.checkItems) {
+				var item = cl.checkItems[ci];
+				
+				if ( item.state == "complete" ) {
+					++comp;
+				}
+			}
+			
+			if ( type == 0 ) {
+				return comp+'/'+count;
+			}
+			
+			return Math.round(comp/count*100);
+		}
 	};
 	
 	return svc;
